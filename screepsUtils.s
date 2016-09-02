79d330e2f637d4312252340ee8d0f4fd07f1e52a
Sriraghavan Subramanian <Sriraghavan Subramanian>
2016-09-02 13:40:53 +0530

    First Commit with Tutorial Stuff

---
+++ screepsUtils.s
@@ -0,0 +1,11 @@
+// screepsUtils.js
+
+module.exports = {
+	// spawn : which spawn (name) to create creep at. Eg : 'Spawn1'
+	// creepArray : creep description array. Eg : [WORK, CARRY, MOVE]
+	// creepName : 'Harvester1'
+	
+	createCreep : function(spawn, creepArray, creepName) { 
+		Game.spawns[spawn].createCreep( creepArray, creepName );
+	},
+};
